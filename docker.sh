bce49c5586030d5fd3806a10ee1032a1d18dffbe
Amine <amine.benhamza@gmail.com>
2019-01-06 20:38:46 +0100

    adding docker.sh

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Docker installation
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce
+
+#Executing the Docker Command Without Sudo
+sudo usermod -aG docker ${USER}
+su - ${USER}
